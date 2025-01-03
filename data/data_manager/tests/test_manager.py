2afb9c772ee497cb3d45cabec5241da054aef5fc
Sauravroy34 <sauravnotfound29@gmail.com>
2025-01-04 00:28:57 +0530

    hksg

--- data/data_manager/tests/test_manager.py
+++ data/data_manager/tests/test_manager.py
@@ -16,7 +16,7 @@ def test_override_file(manager, data_function, tmpdir):
         """
         Function to test whether the file is /tmp/another_file.
         """
-        assert str(manager.get('test_file')) == (f"{folder}/another_file")
+        assert Path(manager.get('test_file')) == Path(f"{folder}/another_file")
 
 
     # Outside the context manager file is default
